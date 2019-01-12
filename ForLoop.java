b3372b6a593d4a4ab626a9d6201bf90c4d5e4c4d
gayathribuddhika <39160904+gayathribuddhika@users.noreply.github.com>
2019-01-12 16:34:06 +0530

    ForLoop
    For Loop

---
+++ ForLoop.java
@@ -0,0 +1,16 @@
+
+public class ForLoop {
+
+    
+    public static void main(String[] args) {
+        int [] array = {25,56,79,53,48};
+        for(int i=0;i<5;i++){
+            System.out.println(array[i]);
+        }
+	/*for(int element:array){
+            System.out.println(element);
+        
+        }*/ //This is the other way of print the elements of the array.
+    }
+    
+}
